bde4f17780e823a45105c2b3b77e1e6d0ba392f8
Jake Soenneker <jake@soenneker.com>
2026-07-27 18:18:29 +0000

    Automated update

--- src/Soenneker.Close.OpenApiClient/User/UserRequestBuilder.cs
+++ src/Soenneker.Close.OpenApiClient/User/UserRequestBuilder.cs
@@ -53,7 +53,7 @@ namespace Soenneker.Close.OpenApiClient.User
         {
         }
         /// <summary>
-        /// List all the users who are members of the same organizations as you are.
+        /// List all users who are members of your organization.
         /// </summary>
         /// <returns>A <see cref="global::Soenneker.Close.OpenApiClient.Models.DefaultResponseResponseJson209"/></returns>
         /// <param name="cancellationToken">Cancellation token to use when cancelling requests</param>
@@ -71,7 +71,7 @@ namespace Soenneker.Close.OpenApiClient.User
             return await RequestAdapter.SendAsync<global::Soenneker.Close.OpenApiClient.Models.DefaultResponseResponseJson209>(requestInfo, global::Soenneker.Close.OpenApiClient.Models.DefaultResponseResponseJson209.CreateFromDiscriminatorValue, default, cancellationToken).ConfigureAwait(false);
         }
         /// <summary>
-        /// List all the users who are members of the same organizations as you are.
+        /// List all users who are members of your organization.
         /// </summary>
         /// <returns>A <see cref="RequestInformation"/></returns>
         /// <param name="requestConfiguration">Configuration for the request such as headers, query parameters, and middleware options.</param>
@@ -99,7 +99,7 @@ namespace Soenneker.Close.OpenApiClient.User
             return new global::Soenneker.Close.OpenApiClient.User.UserRequestBuilder(rawUrl, RequestAdapter);
         }
         /// <summary>
-        /// List all the users who are members of the same organizations as you are.
+        /// List all users who are members of your organization.
         /// </summary>
         [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
         public partial class UserRequestBuilderGetQueryParameters 
