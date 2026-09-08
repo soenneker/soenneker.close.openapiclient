68f0a4fc2dfa8b15e2dabb4ce1309bd295b5b595
Jake Soenneker <jake@soenneker.com>
2026-09-08 19:40:30 +0000

    Automated update

--- src/Soenneker.Close.OpenApiClient/Models/SmsActivity.cs
+++ src/Soenneker.Close.OpenApiClient/Models/SmsActivity.cs
@@ -32,6 +32,14 @@ namespace Soenneker.Close.OpenApiClient.Models
 #else
         public string AgentConfigId { get; set; }
 #endif
+        /// <summary>The caller_id_mode property</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public global::Soenneker.Close.OpenApiClient.Models.CallerIdModeWrapper? CallerIdMode { get; set; }
+#nullable restore
+#else
+        public global::Soenneker.Close.OpenApiClient.Models.CallerIdModeWrapper CallerIdMode { get; set; }
+#endif
         /// <summary>The contact_id property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
@@ -274,6 +282,7 @@ namespace Soenneker.Close.OpenApiClient.Models
                 { "activity_at", n => { ActivityAt = n.GetDateTimeOffsetValue(); } },
                 { "agent_action_reason", n => { AgentActionReason = n.GetStringValue(); } },
                 { "agent_config_id", n => { AgentConfigId = n.GetStringValue(); } },
+                { "caller_id_mode", n => { CallerIdMode = n.GetObjectValue<global::Soenneker.Close.OpenApiClient.Models.CallerIdModeWrapper>(global::Soenneker.Close.OpenApiClient.Models.CallerIdModeWrapper.CreateFromDiscriminatorValue); } },
                 { "contact_id", n => { ContactId = n.GetStringValue(); } },
                 { "cost", n => { Cost = n.GetStringValue(); } },
                 { "created_by", n => { CreatedBy = n.GetStringValue(); } },
@@ -318,6 +327,7 @@ namespace Soenneker.Close.OpenApiClient.Models
             writer.WriteDateTimeOffsetValue("activity_at", ActivityAt);
             writer.WriteStringValue("agent_action_reason", AgentActionReason);
             writer.WriteStringValue("agent_config_id", AgentConfigId);
+            writer.WriteObjectValue<global::Soenneker.Close.OpenApiClient.Models.CallerIdModeWrapper>("caller_id_mode", CallerIdMode);
             writer.WriteStringValue("contact_id", ContactId);
             writer.WriteStringValue("cost", Cost);
             writer.WriteStringValue("created_by", CreatedBy);
